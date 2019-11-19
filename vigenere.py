b32d4d3b835e40a98c5c8329852f7ca0260a9d54
Koshsky <57063441+Koshsky@users.noreply.github.com>
2019-11-19 21:51:48 +0400

    Update vigenere.py

--- vigenere.py
+++ vigenere.py
@@ -35,10 +35,11 @@ def cipher(string, KEY):
     """
     Шифровка слова
     """
+    
+    string = str(''.join(string).lower()).replace('ё', 'е')
     KEY = KEY.replace('ё', 'е').lower()
     output = ''
     list_of_keys = []
-    string = str(''.join(string).lower())
     
     if lang(KEY) == 'rus':
         for i in KEY:
@@ -52,21 +53,28 @@ def cipher(string, KEY):
     
     while i != len(string):
         if count < len(list_of_keys):
+            
             if string[i].isalpha():
                 letter = ord(string[i].lower()) + list_of_keys[count]
+                
                 if lang(KEY) == 'rus':
                     max_letter_ord = 1072
                     len_lang = 32
+                    
                 elif lang(KEY) == 'eng':
                     max_letter_ord = 122
                     len_lang = 26
+                    
                 while letter > max_letter_ord:
                     letter -= len_lang
                 output += chr(letter)
+                
             else:
                 output += string[i]
+                
             count += 1
             i += 1
+            
         else:
             count = 0
     return 'Зашифрованный текст: ' + output.lower()
@@ -75,10 +83,11 @@ def decipher(string, KEY):
     """
     Расшифровка слова
     """
+    
+    string = str(''.join(string).lower()).replace('ё', 'е')
     KEY = KEY.replace('ё', 'е').lower()
     output = ''
     list_of_keys = []
-    string = str(''.join(string).lower())
     
     if lang(KEY) == 'rus':
         for i in KEY:
