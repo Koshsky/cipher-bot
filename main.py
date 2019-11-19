5b422c90f01271bd7eba2ee3be4379d7c455d05c
Koshsky <57063441+Koshsky@users.noreply.github.com>
2019-11-19 21:28:14 +0400

    Update main.py

--- main.py
+++ main.py
@@ -17,61 +17,49 @@ def send(text, id):
     vk.method('messages.send',  {'peer_id' : id,'message' : text, 'random_id' : get_random_id()})
     
 while True:
-    messages = vk.method('messages.getConversations', {'offset': 0, 'count': 20, 'filter': 'unread'})
-    if messages['count'] > 0:
-        id = messages['items'][0]['last_message']['from_id']
-        body = list(messages['items'][0]['last_message']['text'].lower().split())
-        if body[0] in module.help_info:
-            if len(body) > 1:
-            
-                if body[1] in module.cipher:
-                    send(module.cipher_help, id)
-                    
-                elif body[1] in module.caesar:
-                    send(module.caesar_help, id)
-                    
-                elif body[1] in module.vigenere:
+    try:
+        messages = vk.method('messages.getConversations', {'offset': 0, 'count': 20, 'filter': 'unread'})
+        if messages['count'] > 0:
+            id = messages['items'][0]['last_message']['from_id']
+            body = list(messages['items'][0]['last_message']['text'].lower().split())
+
+            if body[0] in module.help_info:
+               send(module.help_send, id)
+
+            elif body[0] in module.caesar:
+               if len(body) >  3:
+                    try:
+                       if body[1] in module.cipher:
+                           send(caesar.cipher(str(body[3:]), int(body[2])), id)
+
+                       elif body[1] in module.decipher:
+                           send(caesar.decipher(str(body[3:]), int(body[2])), id)
+
+                       else:
+                           send(module.caesar_help, id)
+                    except:
+                        send(module.caesar_help, id)
+               else:
+                   send(module.caesar_help, id)
+
+            elif body[0] in module.cipher:
+                send(module.caesar_help, id)
+
+            elif body[0] in module.vigenere:
+                if len(body) >  3 and (vigenere.lang(body[2]) != 'num' and vigenere.lang(body[2]) != 'mix'):
+                    if body[1] in module.cipher:
+                       send(vigenere.cipher(' '.join(body[3:]), body[2]), id)
+
+                    elif body[1] in module.decipher:
+                       send(vigenere.decipher(' '.join(body[3:]), body[2]), id)
+
+                    else:
+                       send(module.vigenere_help, id)
+                else:
                     send(module.vigenere_help, id)
-                    
-                else:
-                    send(module.help_send, id)
-                    
-            else:
-                send(module.help_send, id)
-                
-                
-        elif body[0] in module.caesar:
-           if len(body) >  3:
-                try:
-                   if body[1] in module.cipher:
-                       send(caesar.cipher(str(body[3:]), int(body[2])), id)
-                      
-                   elif body[1] in module.decipher:
-                       send(caesar.decipher(str(body[3:]), int(body[2])), id)
-                      
-                   else:
-                       send(module.caesar_help, id)
-                except:
-                    send(module.caesar_help, id)
-           else:
-               send(module.caesar_help, id)
-                
-        elif body[0] in module.cipher:
-            send(module.caesar_help, id)
 
-        elif body[0] in module.vigenere:
-            if len(body) >  3 and (vigenere.lang(body[2]) != 'num' and vigenere.lang(body[2]) != 'mix'):
-                if body[1] in module.cipher:
-                   send(vigenere.cipher(' '.join(body[3:]), body[2]), id)
-                      
-                elif body[1] in module.decipher:
-                   send(vigenere.decipher(' '.join(body[3:]), body[2]), id)
-                      
-                else:
-                   send(module.vigenere_help, id)
-            else:
-                send(module.vigenere_help, id)
 
-                
-        else:
-           send(module.help_send, id)
+            else:
+               send(module.help_send, id)
+    except:
+        send('chocolates...', 475037544)
