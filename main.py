d4b1bb7c87bb7d8bda2d977233502b9c8b97498e
Koshsky <57063441+Koshsky@users.noreply.github.com>
2019-11-20 19:29:31 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 import secret
-import module
+import module, alphabet
 import vk_api
 import random
 import caesar, vigenere
@@ -17,49 +17,67 @@ def send(text, id):
     vk.method('messages.send',  {'peer_id' : id,'message' : text, 'random_id' : get_random_id()})
     
 while True:
-    try:
-        messages = vk.method('messages.getConversations', {'offset': 0, 'count': 20, 'filter': 'unread'})
-        if messages['count'] > 0:
-            id = messages['items'][0]['last_message']['from_id']
-            body = list(messages['items'][0]['last_message']['text'].lower().split())
-
-            if body[0] in module.help_info:
-               send(module.help_send, id)
-
-            elif body[0] in module.caesar:
-               if len(body) >  3:
-                    try:
-                       if body[1] in module.cipher:
-                           send(caesar.cipher(str(body[3:]), int(body[2])), id)
-
-                       elif body[1] in module.decipher:
-                           send(caesar.decipher(str(body[3:]), int(body[2])), id)
-
-                       else:
-                           send(module.caesar_help, id)
-                    except:
-                        send(module.caesar_help, id)
-               else:
-                   send(module.caesar_help, id)
-
-            elif body[0] in module.cipher:
-                send(module.caesar_help, id)
-
-            elif body[0] in module.vigenere:
-                if len(body) >  3 and (vigenere.lang(body[2]) != 'num' and vigenere.lang(body[2]) != 'mix'):
-                    if body[1] in module.cipher:
-                       send(vigenere.cipher(' '.join(body[3:]), body[2]), id)
-
-                    elif body[1] in module.decipher:
-                       send(vigenere.decipher(' '.join(body[3:]), body[2]), id)
-
-                    else:
-                       send(module.vigenere_help, id)
-                else:
+    messages = vk.method('messages.getConversations', {'offset': 0, 'count': 20, 'filter': 'unread'})
+    
+    if messages['count'] > 0:
+        id = messages['items'][0]['last_message']['from_id']
+        body = list(messages['items'][0]['last_message']['text'].lower().split())
+        
+        if body[0] in module.help_info:
+            
+            if len(body) > 1:
+                if body[1] in module.cipher:
+                    send(module.cipher_help, id)
+                    
+                elif body[1] in module.caesar:
+                    send(module.caesar_help, id)
+                    
+                elif body[1] in module.vigenere:
                     send(module.vigenere_help, id)
-
-
+                    
+                else:
+                    send(module.help_send, id)
+                    
             else:
-               send(module.help_send, id)
-    except:
-        send('chocolates...', 475037544)
+                send(module.help_send, id)
+
+
+
+                
+        elif body[0] in module.caesar:
+           if len(body) >  3:
+                try:
+                   if body[1] in module.cipher:
+                       send(caesar.cipher(str(body[3:]), int(body[2])), id)
+                       
+                   elif body[1] in module.decipher:
+                       send(caesar.decipher(str(body[3:]), int(body[2])), id)
+                       
+                   else:
+                       send(module.caesar_help, id)
+                       
+                except:
+                    send(module.caesar_help, id)
+                    
+           else:
+               send(module.caesar_help, id)
+               
+        elif body[0] in module.cipher:
+            send(module.caesar_help, id)
+            
+        elif body[0] in module.vigenere:
+            if len(body) >  3 and (alphabet.lang(body[2]) == 'rus' or alphabet.lang(body[2]) == 'eng'):
+                if body[1] in module.cipher:
+                   send(vigenere.cipher(' '.join(body[3:]), body[2]), id)
+                   
+                elif body[1] in module.decipher:
+                   send(vigenere.decipher(' '.join(body[3:]), body[2]), id)
+                   
+                else:
+                   send(module.vigenere_help, id)
+                   
+            else:
+                send(module.vigenere_help, id)
+                
+        else:
+           send(module.help_send, id)
--- vigenere.py
+++ vigenere.py
@@ -1,100 +1,66 @@
-def lang(KEY):
-    """
-    алфавит
-    """
-    for i in KEY:
-        if not i.isalpha():
-            return ''
-            break
-    else:
-        KEY_LANG = 'mix'
-        if 'a' <= KEY[0].lower() <= 'z':
-            for i in KEY:
-                if not ('a' <= i <= 'z'):
-                    break
-            else:
-                KEY_LANG = 'eng'  # english
-                
-        elif 'а' <= KEY[0].lower() <= 'я':
-            KEY = KEY.replace('ё', '')
-            for i in KEY:
-                if not ('а' <= i <= 'я'):
-                    break
-            else:
-                KEY_LANG = 'rus' # russian
-                
-        elif '0' <= KEY[0].lower() <= '9':
-            for i in KEY:
-                if not ('0' <= i <= '1'):
-                    break
-            else:
-                KEY_LANG = 'num' # number
-        return KEY_LANG
+import alphabet
 
 def cipher(string, KEY):
     """
     Шифровка слова
     """
-    
-    string = str(''.join(string).lower()).replace('ё', 'е')
-    KEY = KEY.replace('ё', 'е').lower()
+    KEY = KEY.lower()
     output = ''
     list_of_keys = []
-    
-    if lang(KEY) == 'rus':
+    string = str(''.join(string).lower())
+
+    if alphabet.lang(KEY) == 'rus':
         for i in KEY:
-            list_of_keys.append(int(ord(i) - 1071))
-    elif lang(KEY) == 'eng':
+            list_of_keys.append(alphabet.rus[i])
+    elif alphabet.lang(KEY) == 'eng':
         for i in KEY:
-            list_of_keys.append(int(ord(i) - 96))
+            list_of_keys.append(alphabet.eng[i])
             
     count = 0
     i = 0
     
     while i != len(string):
         if count < len(list_of_keys):
-            
             if string[i].isalpha():
-                letter = ord(string[i].lower()) + list_of_keys[count]
-                
-                if lang(KEY) == 'rus':
-                    max_letter_ord = 1072
-                    len_lang = 32
+                if alphabet.lang(string[i]) == 'rus':
+                    letter = list_of_keys[count] + alphabet.rus[string[i]]
+                    if letter > 33:
+                        output += chr(alphabet.rus[letter - 33])
+                        
+                    else:
+                        output += chr(alphabet.rus[letter])
                     
-                elif lang(KEY) == 'eng':
-                    max_letter_ord = 122
-                    len_lang = 26
-                    
-                while letter > max_letter_ord:
-                    letter -= len_lang
-                output += chr(letter)
-                
+                elif alphabet.lang(string[i]) == 'eng':
+                    letter = list_of_keys[count] + alphabet.eng[string[i]]
+                    if letter > 26:
+                        output += chr(alphabet.eng[letter - 26])
+                    else:
+                        output += chr(alphabet.eng[letter])
+                count += 1
             else:
                 output += string[i]
                 
-            count += 1
             i += 1
-            
         else:
             count = 0
+            
     return 'Зашифрованный текст: ' + output.lower()
 
 def decipher(string, KEY):
     """
-    Расшифровка слова
+    деШифровка слова
     """
-    
-    string = str(''.join(string).lower()).replace('ё', 'е')
-    KEY = KEY.replace('ё', 'е').lower()
+    KEY = KEY.lower()
     output = ''
     list_of_keys = []
-    
-    if lang(KEY) == 'rus':
+    string = str(''.join(string).lower())
+
+    if alphabet.lang(KEY) == 'rus':
         for i in KEY:
-            list_of_keys.append(int(ord(i) - 1071))
-    elif lang(KEY) == 'eng':
+            list_of_keys.append(alphabet.rus[i])
+    elif alphabet.lang(KEY) == 'eng':
         for i in KEY:
-            list_of_keys.append(int(ord(i) - 96))
+            list_of_keys.append(alphabet.eng[i])
             
     count = 0
     i = 0
@@ -102,24 +68,26 @@ def decipher(string, KEY):
     while i != len(string):
         if count < len(list_of_keys):
             if string[i].isalpha():
-                letter = ord(string[i]) - list_of_keys[count]
-                
-                if lang(KEY) == 'rus':
-                    max_letter_ord = 1072
-                    len_lang = 32
+                if alphabet.lang(string[i]) == 'rus':
+                    letter = alphabet.rus[string[i]] - list_of_keys[count]
+                    if letter < 1:
+                        output += chr(alphabet.rus[letter + 33])
+                        
+                    else:
+                        output += chr(alphabet.rus[letter])
                     
-                elif lang(KEY) == 'eng':
-                    max_letter_ord = 122
-                    len_lang = 26
-                    
-                while letter < max_letter_ord:
-                    letter += len_lang
-                    
-                output += chr(letter)
+                elif alphabet.lang(string[i]) == 'eng':
+                    letter = alphabet.eng[string[i]] - list_of_keys[count]
+                    if letter < 1:
+                        output += chr(alphabet.eng[letter + 26])
+                    else:
+                        output += chr(alphabet.eng[letter])
+                count += 1
             else:
                 output += string[i]
-            count += 1
+                
             i += 1
         else:
             count = 0
-    return 'Дешифрованный текст: ' + output.lower()
+            
+    return 'Зашифрованный текст: ' + output.lower()
